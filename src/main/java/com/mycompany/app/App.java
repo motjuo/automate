cf56c46b08def551fb667dde5008a3002f38b007
motjuo <motiejus.juodelis@gmail.com>
2016-08-29 23:14:13 +0300

    created structure

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Created by motiejus on 8/29/2016.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
